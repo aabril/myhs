6bb04fd6f7e5c0561df410b3fba02f8de25c07f2
[Albert Abril] <albert.abril@gmail.com>
2019-04-01 15:38:57 +0000

    env file

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const upload = multer({ dest: path.join(__dirname, 'uploads') });
 /**
  * Load environment variables from .env file, where API keys and passwords are configured.
  */
-dotenv.config({ path: '.env.example' });
+dotenv.config({ path: '.env.beta.albertabril.com' });
 
 /**
  * Controllers (route handlers).
