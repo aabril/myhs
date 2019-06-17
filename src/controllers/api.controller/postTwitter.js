5fe27f97c9394aafd901f30c80b40a5957264d25
Albert Abril <albert@mbp.connect>
2019-06-17 17:30:17 +0100

    simplify config

--- src/controllers/api.controller/postTwitter.js
+++ src/controllers/api.controller/postTwitter.js
@@ -15,12 +15,13 @@ function postTwitter (req, res, next) {
     }
   
     const token = req.user.tokens.find(token => token.kind === 'twitter');
-    const T = new Twit({
+    const twitConfig = {
       consumer_key: process.env.TWITTER_KEY,
       consumer_secret: process.env.TWITTER_SECRET,
       access_token: token.accessToken,
       access_token_secret: token.tokenSecret
-    });
+    };
+    const T = new Twit(twitConfig);
     T.post('statuses/update', { status: req.body.tweet }, (err) => {
       if (err) { return next(err); }
       req.flash('success', { msg: 'Your tweet has been posted.' });
