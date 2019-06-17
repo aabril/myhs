d5d8b26d981cb2d3ffcffbd77062b60aad5dd8c8
Albert Abril <albert@mbp.connect>
2019-06-17 15:27:34 +0100

    delete script files

--- src/controllers/api.controller/02.script.sh
+++
@@ -1,4 +0,0 @@
-for f in $(cat ./00.list.txt); 
-do 
-  echo "module.exports = $f"
-done
--- src/controllers/api.controller/03.script.sh
+++
@@ -1,9 +0,0 @@
-for f in $(ls); 
-do
-  if [[ $f == *.js ]]
-  then
-    ITEMNAME=$(echo $f | cut -d'.' -f1)
-    echo $ITEMNAME
-    echo "module.exports = $ITEMNAME" >> $f
-  fi
-done
